1ec4dee6233ca28c2afc76aab650b5b6273d3be0
S13G <ayflix0@gmail.com>
2022-10-15 10:11:07 +0100

    register

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from account.forms import LoginForm
+from account.forms import LoginForm, UserRegistrationForm
 
 # Create your views here.
 
@@ -31,3 +31,19 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {"section": "dashboard"})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it
+            new_user = user_form.save(commit=False)
+            # Set the password
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Save the user object
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
