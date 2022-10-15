60fa4bca553a029ec9c46932cd787a248e9b4863
S13G <ayflix0@gmail.com>
2022-10-15 01:27:09 +0100

    first commit and logging in

---
+++ account/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=255)
+    password = forms.CharField(widget=forms.PasswordInput)
